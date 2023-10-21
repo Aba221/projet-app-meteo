2074655b884e25ae51853dda78e570872d10e687
Aba221 <133520968+Aba221@users.noreply.github.com>
2023-10-21 15:37:47 +0000

    Add files via upload

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+// Définition de la fonction getWeatherBackgroundImage 
+function getWeatherBackgroundImage(weatherCondition) {
+  switch (weatherCondition) {
+    case 'Clear':
+      return 'sunny.jpg'; 
+    case 'Clouds':
+      return 'cloudy.jpg'; 
+    case 'Rain':
+      return 'rainy.jpg'; 
+    
+    default:
+      return 'default.jpg'; // Image par défaut
+  }
+}
+
 const WeatherDisplay = ({ weatherData }) => {
   if (!weatherData) {
     return null;
@@ -7,8 +22,17 @@ const WeatherDisplay = ({ weatherData }) => {
 
   const { name, main, weather, wind } = weatherData;
 
+  const weatherCondition = weather[0].main;
+  const backgroundUrl = getWeatherBackgroundImage(weatherCondition);
+
+  const weatherDisplayStyle = {
+    backgroundImage: `url(${backgroundUrl})`,
+    backgroundRepeat: 'no-repeat', 
+    backgroundSize: 'cover', 
+  };
+
   return (
-    <div className="weather-display">
+    <div className="weather-display" style={weatherDisplayStyle}>
       <h2>{name}</h2>
       <div>Température: {main.temp}°C</div>
       <div>Conditions: {weather[0].description}</div>
